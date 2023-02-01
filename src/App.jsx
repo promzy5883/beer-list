be9764345ef0e61f0f37d5544d973960c66aabf9
promise <pebuka351@gmail.com>
2023-02-01 14:21:23 +0100

    C

---
+++ src/App.jsx
@@ -0,0 +1,60 @@
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import useFetch from "./useFetch";
+import { useState } from "react";
+
+function App() {
+  const [link, setLink] = useState(
+    "https://api.punkapi.com/v2/beers?ids=|1|2|3|4|5|6|7|8|9"
+  );
+  const { data, error, loading } = useFetch(link);
+  const datas = [
+    data[0],
+    data[1],
+    data[2],
+    data[3],
+    data[4],
+    data[5],
+    data[6],
+    data[7],
+    data[8],
+  ];
+  if (error) console.log(error);
+  if (loading) return <p>LOADING...</p>;
+  return (
+    <div className="App">
+      <div className="logo">
+        <h2>Beers</h2>
+        <button className="filter">
+          Filter
+          <span
+            onClick={() => {
+              setLink("https://api.punkapi.com/v2/beers?abv_gt=5");
+            }}
+          >
+            Alcohol by Volume
+          </span>
+        </button>
+      </div>
+      <div className="beers">
+        {data?.map((value) => {
+          return (
+            <div key={value.id} className="beerBox">
+              <div
+                style={{ backgroundImage: `url(${value.image_url})` }}
+                className="beerImg"
+              ></div>
+              <p className="beerName">{value.name}</p>
+              <p className="beerTagline">{value.tagline}</p>
+              <p className="beerDescription">{value.description}</p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
+//https://v2.jokeapi.dev/joke/Any {data?.setup + " " + data?.delivery}
---
+++ src/useFetch.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+export default function useFetch(url) {
+  const [data, setData] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    axios
+      .get(url)
+      .then((response) => {
+        setData(response.data);
+      })
+      .catch((err) => {
+        setError(err);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, [url]);
+  return { data, error, loading };
+}
